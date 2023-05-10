ff82d422ab37300179dc1bc069a7a5f5a3471855
Daniel Pilon <Dan-Eli@users.noreply.github.com>
2023-05-10 08:25:03 -0400

    Copy files

--- python/test_ddr_api.py
+++ python/test_ddr_api.py
@@ -8,18 +8,20 @@ This module execute the functionality test on the DDR API.
 import os
 import json
 import yaml
+import emails
 
 
 # Application modules
 
 class Result:
 
-    def __init__(self, req_total, req_failed, assert_total, assert_failed):
+    def __init__(self, req_total, req_failed, assert_total, assert_failed, collection):
 
         self.req_total = req_total
         self.req_failed = req_failed
         self.assert_total = assert_total
         self.assert_failed = assert_failed
+        self.collection = collection
 
         return
 
@@ -41,74 +43,72 @@ class Requests:
 
         return
 
-    def execute_api_call(self, collection=None, request=None):
+    def execute_api_call(self, collection, request):
 
+        
 
-        collection = "test"
-        export_files =  f"--reporter-json-export {collection}.json --reporter-html-export {collection}.html"
-        command = r"newman run C:\Users\dpilon\AppData\Local\Postman\app-10.13.0\DDR_Registry.json -g C:\Users\dpilon\AppData\Local\Postman\app-10.13.0\Globals.json -k -r html,json " + export_files
+        export_files =  f"--reporter-json-export {collection}_file.json --reporter-html-export {collection}_file.html "
+        command = request + " " + export_files
         print (command)
-#        command = r"newman run C:\Users\dpilon\AppData\Local\Postman\app-10.13.0\DDR_Registry.json -g C:\Users\dpilon\AppData\Local\Postman\app-10.13.0\Globals.json -k -r html,json --reporter-json-export test.json --reporter-html-export test.html"
         ret = os.system(command)
         print ("retour =", ret)
 
     def email_result(self):
-        import smtplib
-        from email.mime.multipart import MIMEMultipart
-        from email.mime.text import MIMEText
-        mail_content = '''Hello,
-        This is a simple mail. There is only text, no attachments are there The mail is sent using Python SMTP library.
-        Thank You
-        '''
-        #The mail addresses and password
-        sender_address = 'bergeronpilon@gmail.com'
-        sender_pass = 'DanielGmail'
-        receiver_address = 'bergeronpilon@gmail.com'
-        #Setup the MIME
-        message = MIMEMultipart()
-        message['From'] = sender_address
-        message['To'] = receiver_address
-        message['Subject'] = 'A test mail sent by Python. It has an attachment.'   #The subject line
-        #The body and the attachments for the mail
-        message.attach(MIMEText(mail_content, 'plain'))
-        #Create SMTP session for sending the mail
-        session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
-        session.starttls() #enable security
-        session.login(sender_address, sender_pass) #login with mail_id and password
-        text = message.as_string()
-        session.sendmail(sender_address, receiver_address, text)
-        session.quit()
-        print('Mail Sent')
+
+        # Prepare the email
+        message = emails.html(html="voici le text", subject="Sujet", mail_from="ddr.fgpservices-servicespgf.rdd@aws.nrcan-rncan.cloud")
+
+        # message.cc = config_env.EMAIL_ADMIN
+
+        # Send the email
+        r = message.send(
+            to="bergeronpilon@gmail.com",
+            smtp={
+                "host":"email-smtp.ca-central-1.amazonaws.com",
+                "port": 587,
+                "timeout": 10,
+                "user": "AKIARHK2ZDWAX5LTKCJH",
+                "password": "BCpDDYnkVsrj8gvCTu7LliMq8+ie4C/JB6QZAEByLuVK",
+                "tls": True
+            }
+        )
+
+    def extract_stats(self, collection):
+
+        json_report = collection + "_file.json"
+
+        # Open the JSON report file
+        handle = open(json_report)
+
+        json_report = json.load(handle)
+
+        stats = json_report['run']['stats']
+        req_total = stats['requests']['total']
+        req_failed = stats['requests']['failed']
+
+        assert_total = stats['assertions']['total']
+        assert_failed = stats['assertions']['failed']
+
+        print(req_total, req_failed, assert_total, assert_failed)
+        result = Result(req_total, req_failed, assert_total, assert_failed, collection)
+        return result
 
     def manage_api_testing(self):
         # Read the YAML configuration file
         self.__read_yaml_file()
 
         collections = self.config_yaml["collections"]
+        self.results = []  # Initialize the array of results
         for collection in collections:
             request = self.config_yaml["collections"][collection]
-            # self.execute_api_call(self, collection, request)
+            self.execute_api_call(collection, request)
+            result = self.extract_stats(collection)
+            self.results.append(result)
 
         self.email_result()
 
 
-def extract_content(json_report):
 
-    # Open the JSON report file
-    handle = open(json_report)
-
-    json_data = json.load(handle)
-
-    stats = json_data['run']['stats']
-    req_total = stats['requests']['total']
-    req_failed = stats['requests']['failed']
-
-    assert_total = stats['assertions']['total']
-    assert_failed = stats['assertions']['failed']
-
-    print (req_total, req_failed, assert_total, assert_failed)
-    result = Result(req_total, req_failed, assert_total, assert_failed)
-    return result
 
 
 
@@ -116,17 +116,6 @@ requests = Requests()
 
 requests.manage_api_testing()
 
-## Read the content of the YAML file
-#requests.read_yaml_file()
 
-# Clone the collections
-#requests.clone_collections()
-
-# Read the content of the YAML file
-#requests.execute_api_call()
-
-#extract_content()
-
-#mail_results_()
 
 print
