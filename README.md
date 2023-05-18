754c29c87a582fe45f809cf59e8f8c4aaa3c1df6
Daniel Pilon <Dan-Eli@users.noreply.github.com>
2023-05-18 10:44:47 -0400

    Copy files

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # test_ddr_api
-Functional test for the DDR API
+
+Ce programme a pour but d'exécuter les tests de fonctionnalités des appels des différents API qui sont un des éléments de base de la 
+[Stratégie de tests logiciels de l'équipe du DDR.](https://github.com/federal-geospatial-platform/project_management/tree/main/testing_strategy)
+
+Lorsqu'un logiciel contient avec des point d'entrées original pour une ou plusieurs interfaces de proggramation d'application, 
+il est impératif de créer des tester ces points d'entrées en utilisant la plateforme de tests 
+[Postman.](https://www.postman.com/).  ???Bonnes pratiques pour tester
+
+# Le fichier YMAL
+
+Le programme test_ddr_api est configurable via un YAML. Cette section décrit le contenu de ce fichier.
+
+`mode: internal 
+email: 
+  from: bergeronpilon@gmail.com
+  to: [bergeronpilon@gmail.com]
+  host: email-smtp.ca-central-1.amazonaws.com
+  user: AKIARHK2ZDWAX5LTKCJH
+  password: BCpDDYnkVsrj8gvCTu7LliMq8+ie4C/JB6QZAEByLuVK
+  port: 587
+  timeout: 10
+  tls: True
+log: ../log/API_test.log
+collections:
+  PyGeoAPI:
+    request: newman run ../newman/PyGeoAPI.json -k -r html,json
+    var_url: urlPyGeoApi
+    url_internal: http://10.68.130.138:5000/openapi`
--- python/test_ddr_api.py
+++ python/test_ddr_api.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-This module execute the functionnality test of the API created by Postman..
+This module executes the functionality test of the API created by Postman.
 """
 
 # Imports
@@ -15,6 +15,9 @@ import logging
 # Application modules
 
 class Result:
+    """
+    Class containing the result statistics of one collections.
+    """
 
     def __init__(self, req_total, req_failed, assert_total, assert_failed, collection):
 
@@ -24,9 +27,14 @@ class Result:
         self.assert_failed = assert_failed
         self.collection = collection
 
+        # Initialize the logger
+
         return
 
-class Requests:
+class TestApi:
+    """
+    Main class responsible to execute the API testing.
+    """
 
     def __init__(self):
 
@@ -38,52 +46,48 @@ class Requests:
         return
 
     def __copy_json_documents(self):
+        """
+        Delete the content of the ./files diretory (content of the previous runs)
 
-        # Delete the content of the ./files diretory
+        :return: None
+        """
+
         files = glob.glob('../file/*')
         for file in files:
             os.remove(file)
 
-#        # Copy the JSON collection from the GitHub to the ./files directory
-#        collections = self.config_yaml["collections"]
-#        for collection in collections:
-#            github = collections[collection]['github']
-#            command = f"git clone {github} ../files"
-#            ret = os.system(command)
-
-
-
     def __read_yaml_file(self):
+        """
+        Read the YAML config file and store it in an instance variable
+
+        :return:None
+        """
 
         with open('config.yaml', 'r', encoding='utf-8') as file:
             self.config_yaml = yaml.safe_load(file)
 
-        print(self.config_yaml)
-        collections = self.config_yaml["collections"]
-        for collection in collections:
-            print(self.config_yaml["collections"][collection])
-
         return
 
-    def execute_api_call(self, collection_name, mode):
+    def __execute_api_call(self, collection_name, mode):
+        """
+        This method executes the call the one request API call
 
-        # Initialize the logger
-        logging.basicConfig(filename=self.config_yaml["log"], filemode='a',
-                            level=logging.INFO,
-                            format='%(asctime)s - %(levelname)s - %(message)s',
-                            datefmt="%Y-%m-%d %H:%M:%S",)
+        :param collection_name: Name of the collection
+        :param mode: Internal or External call
+        :return: None
+        """
 
         # Extract the collection to process
         collection = self.config_yaml["collections"][collection_name]
 
-        #Define files
+        #Define file names
         json_file = f"{collection_name}_file.json"
         html_file = f"{collection_name}_file.html"
 
         # Extract the request
         request = collection["request"]
 
-        # Extract
+        # Extract the name
         var_url = collection["var_url"]
         url_internal = collection["url_internal"]
 
@@ -92,17 +96,23 @@ class Requests:
         command = request + " " + export_files + var_collection
         logging.info(command)
         ret = os.system(command)
-        print ("retour =", ret)
 
+        # Store the name of the html and json files
         self.html_files.append(f"../files/{html_file}")  # Add the name of the attached file
         self.json_files.append(f"../files/{json_file}")  # Add the name of the attached file
 
-    def _build_email_body_text(self):
+    def __build_email_body_text(self):
+        """
+        Build the email body text
 
+        :return: None
+        """
 
+        HS = "&nbsp;&nbsp;"  # Hard space
 
-        HS = "&nbsp;&nbsp;"
-        detailed_stats= ""
+        detailed_stats= "<p style=""font-family:'Courier New'"">"  # Contained the detailed statistics
+
+        # Loop over each the result of each collection
         for result in self.results:
             if result.req_failed == 0 and result.assert_failed == 0:
                 status_fr = "Succès"
@@ -110,33 +120,46 @@ class Requests:
             else:
                 status_fr = "Échec"
                 status_en = "Failure"
-            self.tilte = f"{status_fr} - Résultat des test de l'API / {status_en} - Result from API test <br>"
+            self.tilte = f"{status_fr} - Résultat des tests de l'API / {status_en} - Result from API test <br>"
             detailed_stats += f"{HS}- {result.collection}: {status_fr} <br>"
             detailed_stats += f"{HS}{HS}- Requête: succès: {result.req_total - result.req_failed} <br>"
             detailed_stats += f"{HS}{HS}- Requête: échec: {result.req_failed} <br>"
             detailed_stats += f"{HS}{HS}- Assertion: succès: {result.assert_total - result.assert_failed} <br>"
             detailed_stats += f"{HS}{HS}- Assertion: échec: {result.assert_failed} <br><br>"
 
+        # Is the overall API test was a succes
         if self.test_success:
             status_fr = "Succès"
             status_en = "Success"
+            color = "green"  # Color of the Welcome message in the email body
         else:
             status_fr = "Échec"
             status_en = "Failure"
+            color = "red"  # Color of the Welcome message in the email body
+
+        # Build the email body
         self.body = ""
-        self.body += "DDR API Test <br><br>"
+        self.body += "<b>DDR API Test</b> <br><br>"
         self.body += "(English message follows) <br><br>"
-        self.body += "Bonjour,  <br><br>"
-        self.body += f"{status_fr} des tests de fonctionnalités de l'API / {status_en} of the API fonctionality TEST <br><br>"
-        self.body += "Voici les résultats détaillés du tests: <br><br>"
+        self.body += "Bonjour,  <br>"
+        self.body += f'<p style="color: {color}"><b><font size="+2">'  # Set the color of the title according to Success or Failure
+        self.body += f"{status_fr} des tests de fonctionnalités de l'API / {status_en} of the API fonctionality TEST <br>"
+        self.body += '<p style="color: black"></b>'  # Reset color to black
+        self.body += "Voici les résultats détaillés du tests: <br>"
         self.body += detailed_stats
 
-        self.subject = f"{status_fr} des test fonctionalité de l'API / {status_en} of the API functionality test"
+        # Build the email subject
+        self.subject = f"{status_fr} des tests fonctionalité de l'API / {status_en} of the API functionality test"
 
-    def email_result(self):
+    def __email_results(self):
+        """
+        Email the results using the information
+
+        :return: None
+        """
 
         # Build text body
-        self._build_email_body_text()
+        self.__build_email_body_text()
 
         # Prepare the email
         message = emails.html(html=self.body, subject=self.subject, mail_from="ddr.fgpservices-servicespgf.rdd@aws.nrcan-rncan.cloud")
@@ -159,33 +182,53 @@ class Requests:
             }
         )
 
-    def extract_stats(self, collection):
+    def __extract_stats(self, collection):
+        """
+        Read the statistics in the JSON file
 
+        :param collection: Name of the YAML collection to process
+        :return: Class instance containing the result of the class
+        """
+
+        # Extract the name of the JSON file constaining the statistics
         json_report = self.json_files[-1]
 
         # Open the JSON report file
         handle = open(json_report)
 
-        json_report = json.load(handle)
+        json_report = json.load(handle)  # Load the JSON file
 
+        # Extract the content
         stats = json_report['run']['stats']
         req_total = stats['requests']['total']
         req_failed = stats['requests']['failed']
-
         assert_total = stats['assertions']['total']
         assert_failed = stats['assertions']['failed']
 
-        print(req_total, req_failed, assert_total, assert_failed)
+        logging.info(f"Total request: {req_total}; Request failed: {req_failed}; Total assertion: {assert_total}; Assertion failed: {assert_failed}")
         result = Result(req_total, req_failed, assert_total, assert_failed, collection)
 
         if req_failed != 0 or assert_failed != 0:
-            self.test_success = False  # The test failed
+            self.test_success = False  # Set the test to failed
+
         return result
 
-    def manage_api_testing(self, mode):
+    def manage_api_testing(self):
+        """
+        This method manages the api testing by looping over each collection in the YAML, call the API
+
+        :param mode:
+        :return:
+        """
 
         # Read the YAML configuration file
         self.__read_yaml_file()
+
+        # Create the logger
+        logging.basicConfig(filename=self.config_yaml["log"], filemode='a',
+                            level=logging.INFO,
+                            format='%(asctime)s - %(levelname)s - %(message)s',
+                            datefmt="%Y-%m-%d %H:%M:%S", )
 
         # Copy the JSON collection documents
         self.__copy_json_documents()
@@ -193,19 +236,16 @@ class Requests:
         collections = self.config_yaml["collections"]
         for collection_name in collections:
             # Execute the API call using the newman application
-            self.execute_api_call(collection_name, mode)
+            mode = None
+            self.__execute_api_call(collection_name, mode)
 
             # Extract the statistics of the files
-            result = self.extract_stats(collection_name)
+            result = self.__extract_stats(collection_name)
             self.results.append(result)
 
         # Send
-        self.email_result()
+        self.__email_results()
 
 
-requests = Requests()
-
-mode = "internal"
-requests.manage_api_testing(mode)
-
-
+test_api = TestApi()
+test_api.manage_api_testing()
